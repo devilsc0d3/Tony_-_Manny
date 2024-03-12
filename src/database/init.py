0323478fbb78a0f5cf403f2535688f48713e4c4f
leo <leo.faure@ynov.com>
2024-03-12 08:57:01 +0100

    refactor database

--- src/database/init.py
+++ src/database/init.py
@@ -31,33 +31,54 @@ def create_tables():
                                         phone_number INTEGER
                                     ); """
 
-            orders_table = """ CREATE TABLE IF NOT EXISTS orders (
-                                        order_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                        user_id INTEGER NOT NULL,
-                                        click_and_collect_id INTEGER,
-                                        table_reservation_id INTEGER,
-                                        FOREIGN KEY(user_id) REFERENCES users(user_id),
-                                        FOREIGN KEY(click_and_collect_id) REFERENCES click_and_collects(click_and_collect_id),
-                                        FOREIGN KEY(table_reservation_id) REFERENCES table_reservations(table_reservation_id)
-                                    ); """
-
-            click_and_collects_table = """ CREATE TABLE IF NOT EXISTS menu (
-                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+            dishes_table = """ CREATE TABLE IF NOT EXISTS dishes (
+                                    dishe_id INTEGER PRIMARY KEY AUTOINCREMENT,
                                     rank INTEGER NOT NULL,
                                     title VARCHAR(255) NOT NULL,
-                                    recipe VARCHAR(255) NOT NULL
+                                    recipe VARCHAR(255) NOT NULL,
+                                    price FLOAT NOT NULL,
+                                    quantity INTEGER NOT NULL
                                 ); """
 
-            table_reservations_table = """ CREATE TABLE IF NOT EXISTS table_reservations (
-                                table_reservation_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                user_id INTEGER NOT NULL,
-                                FOREIGN KEY(user_id) REFERENCES users(user_id)
+            tables_table = """ CREATE TABLE IF NOT EXISTS tables (
+                                table_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                rank INTEGER NOT NULL,
+                                place INTEGER NOT NULL
                             ); """
+            drinks_table = """ CREATE TABLE IF NOT EXISTS drinks (
+                        drink_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name VARCHAR(255) NOT NULL,
+                        recipe VARCHAR(255) NOT NULL,
+                        quantity INTEGER NOT NULL,
+                        price FLOAT NOT NULL
+                    ); """
+
+            reservations_table = """ CREATE TABLE IF NOT EXISTS reservations (
+                        reservation_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        user_id INTEGER,
+                        table_id INTEGER,
+                        date DATETIME,
+                        FOREIGN KEY (user_id) REFERENCES users (user_id),
+                        FOREIGN KEY (table_id) REFERENCES tables (table_id)
+                    ); """
+
+            click_and_collects_table = """ CREATE TABLE IF NOT EXISTS click_and_collects (
+                        click_and_collect_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        user_id INTEGER,
+                        dish_id INTEGER,
+                        drink_id INTEGER,
+                        date DATETIME,
+                        FOREIGN KEY (user_id) REFERENCES users (user_id),
+                        FOREIGN KEY (dish_id) REFERENCES dishes (dish_id),
+                        FOREIGN KEY (drink_id) REFERENCES drinks (drink_id)
+                    ); """
 
             cursor.execute(users_table)
-            cursor.execute(orders_table)
+            cursor.execute(dishes_table)
+            cursor.execute(tables_table)
+            cursor.execute(drinks_table)
+            cursor.execute(reservations_table)
             cursor.execute(click_and_collects_table)
-            cursor.execute(table_reservations_table)
 
             connection.commit()  # update database
             cursor.close()  # close cursor
@@ -83,4 +104,3 @@ def fill_tables():
     conn.commit()
     conn.close()
     close_database(conn)  # close database
-
