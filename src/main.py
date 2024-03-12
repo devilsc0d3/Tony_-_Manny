7e4997c36bafdf059469a804d48e2fbc248edf63
leo <leo.faure@ynov.com>
2024-03-12 11:23:05 +0100

    add begin of front

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from src.database.init import *
+from src.front.home.home_page import home_page
 
 
 def main():
     create_tables()
     fill_tables()
+    home_page()
 
 
 if __name__ == '__main__':
